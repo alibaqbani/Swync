e5ee36d8c7d57ea9733452d0939811bd96b26d3a
Ali Baqbani <alibaqbani@alis-imac.local>
2018-12-04 09:57:41 +0330

    Initial Commit

---
+++ MonoSync/MonoSync.h
@@ -0,0 +1,19 @@
+//
+//  MonoSync.h
+//  MonoSync
+//
+//  Created by Ali Baqbani on 12/4/18.
+//  Copyright © 2018 Ali Baqbani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MonoSync.
+FOUNDATION_EXPORT double MonoSyncVersionNumber;
+
+//! Project version string for MonoSync.
+FOUNDATION_EXPORT const unsigned char MonoSyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MonoSync/PublicHeader.h>
+
+
